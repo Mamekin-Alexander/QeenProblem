f0339c92f75e3ccc3ce0d6a6a6f8d8effb0bdf43
Mamekin Alexander <mamekin.al@gmail.com>
2020-12-12 22:05:44 +0300

    Complete solution

---
+++ Qeen problem.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include "Type_calculations.h"
+/*--------------Initial class-----------------*/
+template <int32_t N, bool persp, int32_t depth, class ...Args>
+struct Tree {};
+/*---------------------------------------------*/
+
+
+template <int32_t N, int32_t depth, int32_t Iter, class... Args>
+struct Checker {};
+
+template <int32_t N, int32_t depth, int32_t Iter, class LastChosen, class Head, class... Args>
+struct Checker<N, depth, Iter, LastChosen, Head, Args...>
+{
+	static const bool OK = Checker <N, depth, Iter + 1, LastChosen, Args...>::OK && (!Equal<LastChosen, Head>::result) &&
+		(!Equal< Abs<Minus<LastChosen, Head>::result>::result, Number<Iter>>::result);
+};
+
+//If we are out of range in Tree - chain is no perspective
+template <int32_t N, int32_t depth, int32_t Iter, class... Args>
+struct Checker<N, depth, Iter, Number<N + 1>, Args...>
+{
+	static const bool OK = false;
+};
+
+
+//If we are there is only LastChosen queen left - stop, return true
+template <int32_t N, int32_t depth, int32_t Iter, class LastChosen>
+struct Checker<N, depth, Iter, LastChosen>
+{
+	static const bool OK = true;
+};
+
+/*---------Generating state tree-------------*/
+template <int32_t N, bool Persp, int32_t depth, int32_t lastChosen, class ...Args>
+struct Tree<N, Persp, depth, Number<lastChosen>, Args...> {
+	static const bool DepthPersp = Persp && Checker<N, depth + 1, 1 , Number<1>, Number<lastChosen>, Args...>::OK;
+	static const bool WidthPersp = Checker<N, depth, 1, Number<lastChosen+1>, Args...>::OK;
+	static const int32_t resultWidth = Tree<N, WidthPersp, depth, Number<lastChosen + 1>, Args... >::result;
+	static const int32_t resultDepth = Tree<N, DepthPersp, depth + 1, Number<1>, Number<lastChosen>, Args...>::result;
+	static const int32_t result = resultDepth + resultWidth;
+};
+
+
+//If chain is not perspective and we can't move in width - stop
+template <int32_t N, int32_t depth, class ...Args>
+struct Tree<N, false, depth, Number<N>, Args...> {
+	static const int32_t result = 0;
+};
+
+//If chain is not perspective and we crossed the border - stop
+template <int32_t N, int32_t depth, class ...Args>
+struct Tree<N, false, depth, Number<N+1>, Args...> {
+	static const int32_t result = 0;
+};
+//If chain is not perspective but we can move in width - move only in width
+template <int32_t N, int32_t depth, int32_t lastChosen, class ...Args>
+struct Tree<N, false, depth, Number<lastChosen>, Args...> {
+	static const bool WidthPersp = Checker<N, depth, 1, Number<lastChosen + 1>, Args...>::OK;
+	static const int32_t result = Tree<N, WidthPersp, depth, Number<lastChosen + 1>, Args... >::result;
+};
+
+//If chain is perspective and desk is complete - we found solution, stop
+template <int32_t N, int32_t lastChosen, class ...Args>
+struct Tree<N, true, N, Number<lastChosen>,  Args...>
+{
+	static const int32_t result = 1;
+};
+
+/*---------------------------------------*/
+
+
+
+
+
+
+int main()
+{
+	constexpr int32_t N = 8;
+	std::cout << Tree<N, true, 1, Number<1>>::result;
+}
---
+++ Type_calculations.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <stdint.h>
+template <int32_t I>
+struct Number {
+	static const int32_t value = I;
+};
+
+template<class U>
+struct Increment {};
+template <int32_t I>
+struct Increment< Number<I> > {
+	typedef Number<I + 1> result;
+};
+
+template<class U>
+struct IsNegative {};
+template <int32_t I>
+struct IsNegative< Number<I> > {
+	static const bool result = (I>0) ? false: true;
+};
+
+template<class U, bool isNeg = IsNegative<U>::result>
+struct Abs {};
+
+template <int32_t I>
+struct Abs< Number<I>,  true > {
+	typedef Number<-I> result;
+};
+template <uint32_t I>
+struct Abs< Number<I>, false > {
+	typedef Number<I> result;
+};
+
+template <class U, class V>
+struct Sum
+{};
+template <int32_t L, int32_t R>
+struct Sum<Number<L>, Number<R>>
+{
+	typedef Number<L + R> result;
+};
+
+template <class U, class V>
+struct Minus{};
+template <int32_t L, int32_t R>
+struct Minus<Number<L>, Number<R>>
+{
+	typedef Number<L - R> result;
+};
+
+template <class U, class V>
+struct Equal
+{};
+template <int32_t L, int32_t R>
+struct Equal<Number<L>, Number<R>>
+{
+	static const bool result = (L == R);
+};
